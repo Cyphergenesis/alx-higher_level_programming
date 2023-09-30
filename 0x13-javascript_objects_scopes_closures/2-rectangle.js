02ff9a989078dad169a04ac326fee1723629f5b7
Cyphergenesis <josquilani200@gmail.com>
2023-09-30 19:15:09 +0100

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,14 @@
+#!/usr/bin/node
+
 class Rectangle {
   constructor(w, h) {
-    if (w > 0 && h > 0) {
+    if (w <= 0 || h <= 0) {
+      // If w or h is not a positive integer, create an empty object
+      return {};
+    } else {
+      // Initialize instance attributes width and height
       this.width = w;
       this.height = h;
-    } else {
-      // If w or h is not a positive integer or equal to 0, create an empty object
-      return {};
     }
   }
 }
